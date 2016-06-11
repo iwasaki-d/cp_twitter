0853d5a73d4dcff500d5169b539534e723acf9a7
dev <dev@iwasaki-no-iMac.local>
2016-06-11 16:11:35 +0900

    UsersControllerを追加しログイン後ページを作成した

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  before_action :set_user
+  before_action :authenticate_current_user
+
+  def show
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def authenticate_current_user
+    unless @user == current_user
+      redirect_to root_path
+    end
+  end
+
+end
