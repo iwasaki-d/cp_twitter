8cf9fb79d4b0fc937833858b81dfd6dd60ce5544
dev <dev@iwasaki-no-iMac.local>
2016-06-11 10:17:28 +0900

    トップページにログインフォームを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
+  before_action :set_user, only: [:index]
 
   def index
 
   end
+
+  private
+
+    def set_user
+      @user = current_user || User.new
+    end
 
 end
