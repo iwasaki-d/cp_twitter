e3167621f3d8f84d0961546fbc0967fbfed2aa97
dev <dev@iwasaki-no-iMac.local>
2016-06-14 06:51:22 +0900

    UserモデルのRspecを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context '正常系' do
+      before { @user = build(:user, name: "test01", password: "pass01", password_confirmation: "pass01") }
+
+      it 'nameとpassword、password確認で保存出来ること' do
+        expect(@user.save).to be_truthy
+      end
+    end
+
+    context '異常系' do
+      before { @user = create(:user, name: "test01", password: "pass01", password_confirmation: "pass01") }
+
+      it '同じ名前の登録は許可しないこと' do
+        new_user = build(:user, name: "test01", password: "pass02", password_confirmation: "pass02")
+        expect(new_user.save).to be_falsey
+      end
+    end
+
+  end
 end
