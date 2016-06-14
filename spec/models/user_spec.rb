571cebb0e492c40f434a19b6a4c4e8922c37da0c
dev <dev@iwasaki-no-iMac.local>
2016-06-14 22:01:54 +0900

    Userモデルのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe User, type: :model do
       it 'nameとpassword、password確認で保存出来ること' do
         expect(@user.save).to be_truthy
       end
+
+      it 'nameと最小文字数のpassword、password確認で保存出来ること。' do
+        newuser = build(:user, name: "test01", password: "1", password_confirmation: "1")
+        expect(newuser.save).to be_truthy
+      end
     end
 
     context '異常系' do
@@ -15,6 +20,16 @@ RSpec.describe User, type: :model do
 
       it '同じ名前の登録は許可しないこと' do
         new_user = build(:user, name: "test01", password: "pass02", password_confirmation: "pass02")
+        expect(new_user.save).to be_falsey
+      end
+
+      it '名前なしの登録は許可しないこと' do
+        new_user = build(:user, name: "", password: "pass", password_confirmation: "pass")
+        expect(new_user.save).to be_falsey
+      end
+
+      it 'パスワードなしの登録は許可しないこと' do
+        new_user = build(:user, name: "test01", password: "", password_confirmation: "")
         expect(new_user.save).to be_falsey
       end
     end
