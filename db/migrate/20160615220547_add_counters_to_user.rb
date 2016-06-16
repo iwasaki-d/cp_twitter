b0225a9edd10a6d6c9706aeaedffd33f562b67d7
dev <dev@iwasaki-no-iMac.local>
2016-06-16 12:04:37 +0900

    フォロー数、フォロワー数の集計にCounter_cache機能を利用する

---
+++ db/migrate/20160615220547_add_counters_to_user.rb
@@ -0,0 +1,6 @@
+class AddCountersToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :following_count, :integer, null: false, default: 0
+    add_column :users, :followers_count, :integer, null: false, default: 0
+  end
+end
