2bc74b17c6a0d53277fd99fd6d97c51514007900
dev <dev@iwasaki-no-iMac.local>
2016-06-17 07:57:48 +0900

    appフォルダ以下にreformatを実行

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -62,13 +62,13 @@ class TweetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tweet
-      @tweet = Tweet.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def tweet_params
-      params.require(:tweet).permit(:body, :user_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def tweet_params
+    params.require(:tweet).permit(:body, :user_id)
+  end
 end
