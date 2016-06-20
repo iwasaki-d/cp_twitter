cacde5bcc2eb74bfe0e1cf7d7dd893ff9a9b4129
dev <dev@iwasaki-no-iMac.local>
2016-06-20 23:48:20 +0900

    ページングの表示数を定数化、表示調整。

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,10 +3,11 @@ class SearchesController < ApplicationController
   def search
     # Tweets.ransackではtweetsテーブルに対してuserテーブルがleft_outer_joinでつながるので、tweet.bodyとuser.nameで検索をかけた場合に
     # Tweetsがないユーザーは取得できないことに注意
-    @tweets = Tweet.ransack(ransack_params).result(distinct: true).page(params[:tweets_page]).per(5)
+    @tweets = Tweet.ransack(ransack_params).result(distinct: true).page(params[:tweets_page]).per(Constants::SEARCH_TWEETS_PAR)
 
     # 検索フォームから検索条件の入力を1回でtweetsとuserを検索したいため、tweetの検索条件からuserの検索条件を作成している
-    @users  = User.ransack({name_or_tweets_body_cont: ransack_params[:body_or_user_name_cont]}).result(distinct: true).page(params[:users_page]).per(2)
+    @users  = User.ransack({name_or_tweets_body_cont: ransack_params[:body_or_user_name_cont]}).result(distinct: true)
+                .page(params[:users_page]).per(Constants::SEARCH_USERS_PAR)
   end
 
   private
---
+++ config/initializers/constants.rb
@@ -0,0 +1,4 @@
+module Constants
+  SEARCH_USERS_PAR = 3
+  SEARCH_TWEETS_PAR = 10
+end
