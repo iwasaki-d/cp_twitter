8d52fe47ef6c845507f0e8181c05a4fb63e4c841
dev <dev@iwasaki-no-iMac.local>
2016-07-02 00:19:22 +0900

    githubに反映させたのでテストコードを削除

--- app/channels/user_notice_channel.rb
+++ app/channels/user_notice_channel.rb
@@ -1,11 +1,5 @@
 class UserNoticeChannel < ApplicationCable::Channel
   def subscribed
     stream_from "#{current_user.id}_notice_channel"
-    stream_from "user_notice_channel"
-  end
-
-  def send_notice(data)
-    sanitized_notice = ERB::Util.html_escape(data['notice'])
-    ActionCable.server.broadcast 'user_notice_channel', notice_html: " <p> #{sanitized_notice}さんのタイムラインが更新されました。</p> "
   end
 end
