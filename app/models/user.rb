59d6806006763d6b8072781d43352d7a276f1f19
dev <dev@iwasaki-no-iMac.local>
2016-07-02 23:02:42 +0900

    リコメンドをクラスメソッドとして呼び出すケースがないためインスタンスメソッドへ。

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,9 +47,9 @@ class User < ActiveRecord::Base
       .eager_load(:user).order_latest.page(load_page).per(Constants::DEFAULT_TWEETS_PAR)
   end
 
-  def self.recommend(recommended_user)
+  def recommend
     # RANDOMはSQLLITE,POSTGRESQL依存。かつ、ほとんどのUSERのレコードが対象になりランダムに取得する重い処理なのでUsersの全レコード数に注意すること。今回はコピーサービスなのでそのまま
-    User.where('NOT EXISTS ( SELECT * FROM relationships a WHERE users.id = a.following_user_id AND a.user_id = ?) AND users.id <> ? ', recommended_user, recommended_user)
+    User.where('NOT EXISTS ( SELECT * FROM relationships a WHERE users.id = a.following_user_id AND a.user_id = ?) AND users.id <> ? ', self.id, self.id)
       .order('RANDOM()').limit(Constants::RECOMMEND_USER_AMOUNT)
   end
 
