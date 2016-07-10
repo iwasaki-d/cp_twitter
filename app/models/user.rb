0250ead99c8ce88a056f1489434f10d145e8a634
dev <dev@iwasaki-no-iMac.local>
2016-07-11 07:34:19 +0900

    ログインの判定をnameだけで行う記述に変更（変更前はemailとnameのどちらでも利用できる記述かつ不完全で機能してなかった）

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,10 +26,7 @@ class User < ActiveRecord::Base
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      #認証の条件式を変更する
-      where(conditions).where(["name = :value", {value: name}]).first
-    else
+    if conditions.has_key?(:name)
       where(conditions).first
     end
   end
