a4ac7406a4fbfe868cac8c1c918e185a290ef8e3
dev <dev@iwasaki-no-iMac.local>
2016-07-10 23:43:43 +0900

    モデルでしか使わない定数をモデル側に移動

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,10 @@ class User < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+  RECOMMEND_USER_AMOUNT = 5
+  MIN_IMAGE_HEIGHT_PIXELS = 200
+  MIN_IMAGE_WIDTH_PIXELS = 200
+
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
@@ -50,7 +54,7 @@ class User < ActiveRecord::Base
   def recommend
     # RANDOMはSQLLITE,POSTGRESQL依存。かつ、ほとんどのUSERのレコードが対象になりランダムに取得する重い処理なのでUsersの全レコード数に注意すること。今回はコピーサービスなのでそのまま
     User.where('NOT EXISTS ( SELECT * FROM relationships a WHERE users.id = a.following_user_id AND a.user_id = ?) AND users.id <> ? ', self.id, self.id)
-      .order('RANDOM()').limit(Constants::RECOMMEND_USER_AMOUNT)
+      .order('RANDOM()').limit(RECOMMEND_USER_AMOUNT)
   end
 
   private
@@ -59,10 +63,10 @@ class User < ActiveRecord::Base
     return true unless self.image.file
 
     dimensions = self.image.dimensions
-    if dimensions && (dimensions[:width] < Constants::MIN_IMAGE_WIDTH_PIXELS || dimensions[:height] < Constants::MIN_IMAGE_HEIGHT_PIXELS)
+    if dimensions && (dimensions[:width] < MIN_IMAGE_WIDTH_PIXELS || dimensions[:height] < MIN_IMAGE_HEIGHT_PIXELS)
       Rails.logger.info "Image upload dimensions: #{dimensions[:width]}x#{dimensions[:height]}"
-      errors.add :image, "#{Constants::MIN_IMAGE_WIDTH_PIXELS}x"+
-        "#{Constants::MIN_IMAGE_HEIGHT_PIXELS}ピクセル以上のサイズの画像をアップロードしてください"+
+      errors.add :image, "#{MIN_IMAGE_WIDTH_PIXELS}x"+
+        "#{MIN_IMAGE_HEIGHT_PIXELS}ピクセル以上のサイズの画像をアップロードしてください"+
         "(現在：#{dimensions[:width]}x#{dimensions[:height]})"
     end
   end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,20 +3,27 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
+  MAX_IMAGE_HEIGHT_PIXELS = 800
+  MAX_IMAGE_WIDTH_PIXELS = 800
+
+  THUMB_IMAGE_HEIGHT_PIXELS = 80
+  THUMB_IMAGE_WIDTH_PIXELS = 80
+
   storage :file
 
-  process resize_to_limit: [Constants::MAX_IMAGE_WIDTH_PIXELS, Constants::MAX_IMAGE_HEIGHT_PIXELS]
+  process resize_to_limit: [MAX_IMAGE_WIDTH_PIXELS, MAX_IMAGE_HEIGHT_PIXELS]
 
   process :dimensions
 
   process convert: 'jpg'
+
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   version :thumb do
-    process :resize_to_fit => [Constants::THUMB_IMAGE_WIDTH_PIXELS, Constants::THUMB_IMAGE_HEIGHT_PIXELS]
+    process :resize_to_fit => [THUMB_IMAGE_WIDTH_PIXELS, THUMB_IMAGE_HEIGHT_PIXELS]
   end
 
   def extension_white_list
