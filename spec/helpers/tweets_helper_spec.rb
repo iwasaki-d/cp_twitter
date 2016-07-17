138be1555f1d8a864ccfc8a748b6a7bc68007f87
dev <dev@iwasaki-no-iMac.local>
2016-07-17 15:46:30 +0900

    テストをrspecらしく変更

--- spec/helpers/tweets_helper_spec.rb
+++ spec/helpers/tweets_helper_spec.rb
@@ -1,23 +1,21 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the CommentsHelper. For example:
-#
-# describe CommentsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TweetsHelper, type: :helper do
   describe 'initialize_remains_body_characters' do
-    it '典型的なユーザー名' do
-      expect(helper.initialize_remains_body_characters('@user1')).to eq(134)
+    subject { helper.initialize_remains_body_characters(body) }
+
+    context '入力がある場合' do
+      let(:body) { '@user1' }
+      it '入力可能な残り文字数を返すこと' do
+        is_expected.to eq(134)
+      end
     end
 
-    it 'ユーザー名入力がなかった場合' do
-      expect(helper.initialize_remains_body_characters('')).to eq(140)
+    context '入力がない場合' do
+      let(:body) { '' }
+      it '入力可能な残り文字数として最大文字数を返すこと' do
+        is_expected.to eq(140)
+      end
     end
   end
 end
